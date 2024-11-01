93f8a35dd58a16d55ea1585d0ddbfc13b7136e90
Marco Santos <marco@marcosantos.dev>
2024-11-01 16:03:05 -0300

    vercel config

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -9,6 +9,18 @@ import { Button } from "~/components/ui/button";
 import { Link } from "~/components/ui/link";
 import { createClient } from "~/lib/supabase/client";
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
+};
+
 export default function SignInPage() {
   const supabase = createClient();
 
@@ -16,7 +28,7 @@ export default function SignInPage() {
     void supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: getURL() + "auth/callback",
       },
     });
   };
