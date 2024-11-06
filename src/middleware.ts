d44c83444775f58850ded84a6a41ab9d599eae9c
Marco Santos <marco@marcosantos.dev>
2024-11-06 17:53:00 -0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|embed|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|embed|api/cron/check-meetings|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
