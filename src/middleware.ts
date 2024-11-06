0cf02c0fc104e8aa5a7c661b403132dbc2290830
Marco Santos <marco@marcosantos.dev>
2024-11-05 23:39:50 -0300

    fix: ignore `embed` on auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { type NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
 
 import { updateSession } from "~/lib/supabase/middleware";
 
@@ -8,6 +8,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|embed|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
