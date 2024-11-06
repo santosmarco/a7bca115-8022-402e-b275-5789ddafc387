cae2aa999e3451c42c8f52c2b19181c486d5f02b
Marco Santos <marco@marcosantos.dev>
2024-11-05 23:38:39 -0300

    feat: improve notion endpoints

--- src/server/api/routers/notion.ts
+++ src/server/api/routers/notion.ts
@@ -5,36 +5,53 @@ import { notion, notionApi } from "~/lib/notion/client";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
+async function getNotionPage(pageId: string) {
+  return await notionApi.getPage(pageId);
+}
+
+async function getEnrichedReportPages<
+  T extends Awaited<ReturnType<typeof notion.databases.query>>,
+>(results: T["results"]) {
+  return Promise.all(
+    results
+      .filter(
+        (
+          result,
+        ): result is Extract<
+          typeof result,
+          { object: "page"; properties: unknown }
+        > => result.object === "page" && "properties" in result,
+      )
+      .map(async (result) => ({
+        ...result,
+        ...(await getNotionPage(result.id)),
+      })),
+  );
+}
+
 export const notionRouter = createTRPCRouter({
+  listAll: publicProcedure.query(async () => {
+    const reports = await notion.databases.query({
+      database_id: env.NOTION_REPORTS_DATABASE_ID,
+    });
+    return getEnrichedReportPages(reports.results);
+  }),
+
   listByClient: publicProcedure
-    .input(z.object({ name: z.string() }))
+    .input(z.object({ name: z.string().nullish() }))
     .query(async ({ input }) => {
-      if (!input.name) return [];
       const reports = await notion.databases.query({
         database_id: env.NOTION_REPORTS_DATABASE_ID,
-        filter: { property: "Client", select: { equals: input.name } },
+        ...(input.name && {
+          filter: { property: "Client", select: { equals: input.name } },
+        }),
       });
-      const reportPages = await Promise.all(
-        reports.results
-          .filter(
-            (
-              result,
-            ): result is Extract<
-              typeof result,
-              { object: "page"; properties: unknown }
-            > => result.object === "page" && "properties" in result,
-          )
-          .map(async (result) => ({
-            ...result,
-            ...(await notionApi.getPage(result.id)),
-          })),
-      );
-      return reportPages;
+      return getEnrichedReportPages(reports.results);
     }),
 
   getOne: publicProcedure
     .input(z.object({ reportId: z.string() }))
     .query(async ({ input }) => {
-      return await notionApi.getPage(input.reportId);
+      return await getNotionPage(input.reportId);
     }),
 });
