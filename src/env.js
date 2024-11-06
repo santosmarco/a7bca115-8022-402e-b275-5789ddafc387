5cf564acf3acd6afb1ff009a4d9fd1b6d5e1a788
Marco Santos <marco@marcosantos.dev>
2024-11-05 23:45:27 -0300

    fix: fix env vars

--- src/env.js
+++ src/env.js
@@ -7,24 +7,56 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    // Vercel
+    VERCEL_URL: z
+      .string()
+      .url()
+      .optional()
+      .refine((url) => process.env.NODE_ENV !== "production" || url),
+    SITE_URL: z
+      .string()
+      .url()
+      .optional()
+      .refine((url) => process.env.NODE_ENV !== "production" || url),
     // API Video
     API_VIDEO_API_KEY: z.string(),
+    // Meeting Baas
+    MEETING_BAAS_API_KEY: z.string(),
     // Notion
     NOTION_INTEGRATION_SECRET: z.string(),
     NOTION_REPORTS_DATABASE_ID: z.string(),
   },
 
   client: {
+    // Vercel
+    NEXT_PUBLIC_VERCEL_URL: z
+      .string()
+      .url()
+      .optional()
+      .refine((url) => process.env.NODE_ENV !== "production" || url),
+    NEXT_PUBLIC_SITE_URL: z
+      .string()
+      .url()
+      .optional()
+      .refine((url) => process.env.NODE_ENV !== "production" || url),
     // Supabase
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
   },
 
   runtimeEnv: {
+    // Server
     NODE_ENV: process.env.NODE_ENV,
+    VERCEL_URL: process.env.VERCEL_URL,
+    SITE_URL: process.env.SITE_URL,
     API_VIDEO_API_KEY: process.env.API_VIDEO_API_KEY,
     NOTION_INTEGRATION_SECRET: process.env.NOTION_INTEGRATION_SECRET,
     NOTION_REPORTS_DATABASE_ID: process.env.NOTION_REPORTS_DATABASE_ID,
+    MEETING_BAAS_API_KEY: process.env.MEETING_BAAS_API_KEY,
+
+    // Client
+    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
