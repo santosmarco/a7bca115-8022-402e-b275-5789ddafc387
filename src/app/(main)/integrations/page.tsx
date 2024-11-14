08631dbec74cb9023336f3398544df1a17a59a65
Marco Santos <marco@marcosantos.dev>
2024-11-14 16:33:36 -0300

    fix: fix page renderings

--- src/app/(main)/integrations/page.tsx
+++ src/app/(main)/integrations/page.tsx
@@ -1,17 +1,5 @@
-import { redirect } from "next/navigation";
-
-import { api } from "~/trpc/server";
-
 import { IntegrationsPageClient } from "./page.client";
 
 export default async function IntegrationsPage() {
-  let user: Awaited<ReturnType<typeof api.auth.getUser>> | null = null;
-  try {
-    user = await api.auth.getUser();
-  } catch (error) {
-    console.error("Failed to get user:", error);
-    redirect("/login");
-  }
-
-  return <IntegrationsPageClient user={user} />;
+  return <IntegrationsPageClient />;
 }
