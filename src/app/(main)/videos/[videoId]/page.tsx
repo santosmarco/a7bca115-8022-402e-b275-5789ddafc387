ab55824c0e80252f1e14a16bfa7a6371b6e88ea9
Marco Santos <marco@marcosantos.dev>
2024-11-14 16:31:40 -0300

    fix: remove sorting

--- src/app/(main)/videos/[videoId]/page.tsx
+++ src/app/(main)/videos/[videoId]/page.tsx
@@ -1,16 +1,9 @@
-import { getVTT } from "~/lib/api-video/videos";
-import { api } from "~/trpc/server";
-
 import { VideoPageClient } from "./page.client";
 
 export default async function VideoPage({
   params,
 }: {
-  params: Promise<{ videoId: string }>;
+  params: { videoId: string };
 }) {
-  const { videoId } = await params;
-  const video = await api.videos.getOne({ videoId });
-  const vtt = await getVTT(video.videoId, "en");
-
-  return <VideoPageClient video={video} vtt={vtt.content} />;
+  return <VideoPageClient videoId={params.videoId} />;
 }
