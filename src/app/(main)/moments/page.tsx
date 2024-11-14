35d4cda3e3c04f3142356d5c83a474322cc3ae7f
Marco Santos <marco@marcosantos.dev>
2024-11-14 16:31:32 -0300

    fix: fix page renderings

--- src/app/(main)/moments/page.tsx
+++ src/app/(main)/moments/page.tsx
@@ -1,11 +1,5 @@
-import { api } from "~/trpc/server";
-
 import { MomentsPageClient } from "./page.client";
 
 export default async function MomentsPage() {
-  const data = await api.videos.listAll({
-    limit: 50, // @todo: optimize
-  });
-
-  return <MomentsPageClient data={data} />;
+  return <MomentsPageClient />;
 }
