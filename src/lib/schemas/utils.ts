4a5f44de02e903f9d3acfbba342de6637b2e9045
Marco Santos <marco@marcosantos.dev>
2024-11-21 07:13:14 +0100

    feat: add `JsonValue` util schema

--- src/lib/schemas/utils.ts
+++ src/lib/schemas/utils.ts
@@ -5,3 +5,18 @@ export const JsonDate = z
   .datetime()
   .or(z.date().transform((d) => d.toISOString()));
 export type JsonDate = z.infer<typeof JsonDate>;
+
+export type JsonValue =
+  | string
+  | number
+  | boolean
+  | null
+  | JsonValue[]
+  | { [key: string]: JsonValue };
+export const JsonValue: z.ZodType<JsonValue> = z.lazy(() =>
+  z.union([
+    z.union([z.string(), z.number(), z.boolean(), z.null()]),
+    z.array(JsonValue),
+    z.record(JsonValue),
+  ]),
+);
