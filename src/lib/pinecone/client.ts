a4cb22918724dd98da248ef41922f24e55dbd14e
Marco Santos <marco@marcosantos.dev>
2024-12-15 15:25:00 -0300

    chore: add `IndexMetadata` schema

--- src/lib/pinecone/client.ts
+++ src/lib/pinecone/client.ts
@@ -1,6 +1,28 @@
 import { Pinecone } from "@pinecone-database/pinecone";
+import { z } from "zod";
 
 import { env } from "~/env";
+
+export const IndexMetadata = z
+  .object({
+    activity_type: z.string(),
+    api_video_id: z.string(),
+    clear_text: z.string(),
+    end_segment_index: z.number(),
+    moments_id: z.string(),
+    profile_id: z.string(),
+    start_segment_index: z.number(),
+    target_person: z.string(),
+    type: z.string(),
+    version: z.string(),
+  })
+  .partial()
+  .and(
+    z.record(
+      z.union([z.string(), z.number(), z.boolean(), z.array(z.string())]),
+    ),
+  );
+export type IndexMetadata = z.infer<typeof IndexMetadata>;
 
 let pineconeClient: Pinecone | null = null;
 
@@ -13,5 +35,5 @@ export async function getPineconeClient() {
 
 export async function getIndex() {
   const pinecone = await getPineconeClient();
-  return pinecone.index(env.PINECONE_INDEX);
+  return pinecone.index<IndexMetadata>(env.PINECONE_INDEX);
 }
