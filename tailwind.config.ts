3c5517719f9340b01aa73cfa8359c18d763f6a32
Marco Santos <marco@marcosantos.dev>
2024-12-19 11:31:27 -0300

    feat: add Tailwind CSS plugins and update dependencies for improved styling and functionality
    - Added new Tailwind CSS plugins: aspect-ratio, container-queries, forms, and typography.
    - Updated @tiptap/core and related extensions to version 2.10.3.
    - Included tippy.js for enhanced tooltip functionality.
    - Updated package.json and pnpm-lock.yaml to reflect new dependencies.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,7 @@
+import tailwindAspectRatio from "@tailwindcss/aspect-ratio";
+import tailwindContainerQueries from "@tailwindcss/container-queries";
+import tailwindForms from "@tailwindcss/forms";
+import tailwindTypography from "@tailwindcss/typography";
 import vidstack from "@vidstack/react/tailwind.cjs";
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
@@ -10,6 +14,45 @@ const tailwindConfig = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": "hsl(var(--foreground))",
+            "--tw-prose-headings": "hsl(var(--foreground))",
+            "--tw-prose-lead": "hsl(var(--foreground))",
+            "--tw-prose-links": "hsl(var(--foreground))",
+            "--tw-prose-bold": "hsl(var(--foreground))",
+            "--tw-prose-counters": "hsl(var(--foreground))",
+            "--tw-prose-bullets": "hsl(var(--foreground))",
+            "--tw-prose-hr": "hsl(var(--border))",
+            "--tw-prose-quotes": "hsl(var(--foreground))",
+            "--tw-prose-quote-borders": "hsl(var(--border))",
+            "--tw-prose-captions": "hsl(var(--foreground))",
+            "--tw-prose-code": "hsl(var(--foreground))",
+            "--tw-prose-pre-code": "hsl(var(--foreground))",
+            "--tw-prose-pre-bg": "hsl(var(--foreground))",
+            "--tw-prose-th-borders": "hsl(var(--border))",
+            "--tw-prose-td-borders": "hsl(var(--border))",
+
+            "--tw-prose-invert-body": "hsl(var(--foreground))",
+            "--tw-prose-invert-headings": "hsl(var(--foreground))",
+            "--tw-prose-invert-lead": "hsl(var(--foreground))",
+            "--tw-prose-invert-links": "hsl(var(--foreground))",
+            "--tw-prose-invert-bold": "hsl(var(--foreground))",
+            "--tw-prose-invert-counters": "hsl(var(--foreground))",
+            "--tw-prose-invert-bullets": "hsl(var(--foreground))",
+            "--tw-prose-invert-hr": "hsl(var(--border))",
+            "--tw-prose-invert-quotes": "hsl(var(--foreground))",
+            "--tw-prose-invert-quote-borders": "hsl(var(--border))",
+            "--tw-prose-invert-captions": "hsl(var(--foreground))",
+            "--tw-prose-invert-code": "hsl(var(--foreground))",
+            "--tw-prose-invert-pre-code": "hsl(var(--foreground))",
+            "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+            "--tw-prose-invert-th-borders": "hsl(var(--border))",
+            "--tw-prose-invert-td-borders": "hsl(var(--border))",
+          },
+        },
+      },
       fontFamily: {
         sans: ["var(--font-inter)", ...fontFamily.sans],
       },
@@ -109,7 +152,15 @@ const tailwindConfig = {
       },
     },
   },
-  plugins: [tailwindAnimate, scrollbarHide, vidstack],
+  plugins: [
+    tailwindAnimate,
+    scrollbarHide,
+    vidstack,
+    tailwindTypography,
+    tailwindForms,
+    tailwindAspectRatio,
+    tailwindContainerQueries,
+  ],
 } satisfies Config;
 
 export default tailwindConfig;
