4481162a79ffdc9d6dce105878e226211a83c5d1
Marco Santos <marco@marcosantos.dev>
2024-12-19 11:40:00 -0300

    feat: add slash-command-mark

---
+++ src/lib/editor/nodes/slash-command-mark.ts
@@ -0,0 +1,54 @@
+import {
+  Mark,
+  markInputRule,
+  markPasteRule,
+  mergeAttributes,
+} from "@tiptap/core";
+
+import { cn } from "~/lib/utils";
+
+export type SlashCommandMarkOptions = {
+  HTMLAttributes: Record<string, unknown>;
+};
+
+export const SlashCommandMark = Mark.create<SlashCommandMarkOptions>({
+  name: "slashCommand",
+
+  addOptions() {
+    return {
+      HTMLAttributes: {
+        class: cn("rounded-sm", "bg-primary", "px-1"),
+      },
+    };
+  },
+
+  parseHTML() {
+    return [{ tag: "slash-command" }];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return [
+      "span",
+      mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
+      0,
+    ];
+  },
+
+  addInputRules() {
+    return [
+      markInputRule({
+        find: /^\/[a-zA-Z0-9]+$/,
+        type: this.type,
+      }),
+    ];
+  },
+
+  addPasteRules() {
+    return [
+      markPasteRule({
+        find: /^\/[a-zA-Z0-9]+$/g,
+        type: this.type,
+      }),
+    ];
+  },
+});
