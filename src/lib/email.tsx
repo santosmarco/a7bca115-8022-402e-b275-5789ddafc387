4b9ed08187213083acd1f2c46ee65988fa7d4379
Marco Santos <marco@marcosantos.dev>
2025-01-01 15:33:19 -0300

    feat: add `email` to `lib`

---
+++ src/lib/email.tsx
@@ -0,0 +1,23 @@
+import { render } from "@react-email/components";
+import { Resend } from "resend";
+
+import {
+  InvitationEmail,
+  type InvitationEmailProps,
+} from "~/email/invite-user";
+import { env } from "~/env";
+
+const resend = new Resend(env.RESEND_API_KEY);
+
+export async function sendInvitationEmail({ invite }: InvitationEmailProps) {
+  const emailHtml = await render(InvitationEmail({ invite }));
+
+  const data = await resend.emails.send({
+    from: "onboarding@emails.withtitan.com",
+    to: invite.email,
+    subject: "You've Been Invited!",
+    html: emailHtml,
+  });
+
+  return data;
+}
