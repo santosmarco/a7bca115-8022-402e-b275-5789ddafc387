791c135603579247e92d4ab308199ff24b1b0593
Marco Santos <marco@marcosantos.dev>
2025-01-01 15:32:43 -0300

    feat: add `userinvite` schema

---
+++ src/lib/user-invites/schemas.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const UserInviteCreate = z.object({
+  firstName: z.string().min(1, { message: "Required" }),
+  lastName: z.string().optional(),
+  companyName: z.string().optional(),
+  email: z.string().min(1, { message: "Required" }).email({
+    message: "Must be a valid email address",
+  }),
+  userId: z.string(),
+});
+export type UserInviteCreate = z.infer<typeof UserInviteCreate>;
