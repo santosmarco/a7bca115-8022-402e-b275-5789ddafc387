01afeed4207aec150377cce4d607baeabc415c31
Marco Santos <marco@marcosantos.dev>
2025-01-17 15:58:18 -0600

    Revert "fix: add required zoom headers"
    This reverts commit 8de323308c5c11bf2948e0affcc96059bfc530da.

--- next.config.js
+++ next.config.js
@@ -6,20 +6,6 @@ await import("./src/env.js");
 
 import { withLogtail } from "@logtail/next";
 
-// Define CSP Header
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://vod.api.video https://lh3.googleusercontent.com;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
-
 /**
  * @type {import("next").NextConfig}
  */
@@ -34,31 +20,6 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-        ],
-      },
-    ];
   },
 };
 
