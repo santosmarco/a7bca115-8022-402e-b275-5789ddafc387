7b8adcf81d0971799ae7032a9d6f4358a48004f4
Marco Santos <marco@marcosantos.dev>
2025-01-27 15:05:30 -0600

    fix(ENG-122): Fix front-end to prevent the rendering of tags that contain the profile ID (not human-readable)

--- src/app/(main)/videos/[videoId]/_components/video-tags.tsx
+++ src/app/(main)/videos/[videoId]/_components/video-tags.tsx
@@ -1,8 +1,9 @@
 import { TagIcon } from "lucide-react";
+import { z } from "zod";
 
 import { Badge } from "~/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import { type Video } from "~/lib/schemas/video";
+import type { Video } from "~/lib/schemas/video";
 
 export type VideoTagsProps = {
   video: Video;
@@ -16,18 +17,24 @@ export function VideoTags({ video }: VideoTagsProps) {
       </CardHeader>
       <CardContent className="py-4">
         <div className="flex flex-wrap gap-2">
-          {video.tags?.map((tag, index) => (
-            <Badge
-              key={index}
-              variant="secondary"
-              className="flex items-center gap-1 px-2 py-1"
-            >
-              <TagIcon className="h-3 w-3" />
-              <span className="text-xs">{tag}</span>
-            </Badge>
-          ))}
+          {video.tags
+            ?.filter((tag) => !isUuid(tag))
+            .map((tag, index) => (
+              <Badge
+                key={index}
+                variant="secondary"
+                className="flex items-center gap-1 px-2 py-1"
+              >
+                <TagIcon className="h-3 w-3" />
+                <span className="text-xs">{tag}</span>
+              </Badge>
+            ))}
         </div>
       </CardContent>
     </Card>
   );
+}
+
+function isUuid(tag: string) {
+  return z.string().uuid().safeParse(tag).success;
 }
