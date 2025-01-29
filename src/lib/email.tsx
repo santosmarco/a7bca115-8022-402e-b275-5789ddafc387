ca3ca7aaa9be4743b558813ede735a6475bc1eff
Marco Santos <marco@marcosantos.dev>
2025-01-29 12:37:06 -0600

    feat: add `sendExploreUnlockedEmail`

--- src/lib/email.tsx
+++ src/lib/email.tsx
@@ -2,6 +2,10 @@ import { render } from "@react-email/components";
 import { Resend } from "resend";
 
 import {
+  ExploreUnlockedEmail,
+  type ExploreUnlockedEmailProps,
+} from "~/email/explore-unlocked";
+import {
   InvitationEmail,
   type InvitationEmailProps,
 } from "~/email/invite-user";
@@ -16,6 +20,21 @@ export async function sendInvitationEmail({ invite }: InvitationEmailProps) {
     from: "onboarding@emails.withtitan.com",
     to: invite.email,
     subject: "Create Your Account On Titan GameTape",
+    html: emailHtml,
+  });
+
+  return data;
+}
+
+export async function sendExploreUnlockedEmail({
+  profile,
+}: ExploreUnlockedEmailProps) {
+  const emailHtml = await render(ExploreUnlockedEmail({ profile }));
+
+  const data = await resend.emails.send({
+    from: "onboarding@emails.withtitan.com",
+    to: profile.email,
+    subject: "🎉 Congrats! Start Exploring Your Performance with GameTape",
     html: emailHtml,
   });
 
